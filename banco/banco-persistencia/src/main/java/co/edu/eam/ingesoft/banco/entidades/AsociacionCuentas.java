916edef66c707210e4cd7ce0e7684064bc30ee3c
LauraPosada <posadalaura998@gmail.com>
2017-04-14 13:17:53 -0500

    asociacion

--- banco/banco-persistencia/src/main/java/co/edu/eam/ingesoft/banco/entidades/AsociacionCuentas.java
+++ banco/banco-persistencia/src/main/java/co/edu/eam/ingesoft/banco/entidades/AsociacionCuentas.java
@@ -8,10 +8,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
-<<<<<<< HEAD
-=======
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
->>>>>>> refs/remotes/origin/master
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -19,54 +19,36 @@ import javax.persistence.Table;
 import org.hibernate.annotations.ManyToAny;
 
 @Entity
-@Table(name = "Asociacion_Cuentas")
-@NamedQueries({
-		@NamedQuery(name = AsociacionCuentas.LISTRA_ASOCIACIONES, query = "SELECT a FROM AsociacionCuentas a WHERE a.cliente=?1"),
-		@NamedQuery(name = AsociacionCuentas.LISTA_ASOCIACIONES_COMFIRMADAS, query="SELECT a FROM AsociacionCuentas a WHERE a.cliente=?1 AND a.verificado='Asociada'"),
-		})
-public class AsociacionCuentas implements Serializable {
-
+@Table(name="Asociacion_Cuentas")
+@NamedQuery(name= AsociacionCuentas.LISTRA_ASOCIACIONES, query = "SELECT a FROM AsociacionCuentas a WHERE a.cliente=?1")
+public class AsociacionCuentas  implements Serializable{
+	
 	public static final String LISTRA_ASOCIACIONES = "AsociacionCuentas.listar";
-	public static final String LISTA_ASOCIACIONES_COMFIRMADAS = "AsociacionCuentas.listaConfirmada";
-
+	
 	@Id
-	@Column(name = "numero_Id")
+	@Column(name="numero_Id")
 	private String numeroId;
 
-	@Column(name = "Tipo_Id")
+	@Column(name="Tipo_Id")
 	private String tipoId;
-
-	@Column(name = "nombre")
+	
+	@Column(name="nombre")
 	private String nombreTitular;
-<<<<<<< HEAD
 	
 	@ManyToOne
 	@JoinColumn(name="banco_i")
 	private Banco banco;
 	
 	@Column(name="Numero")
-=======
-
-	@ManyToOne
-	@JoinColumn(name = "Banco")
-	private  Banco banco;
-
-	@Column(name = "Numero")
->>>>>>> refs/remotes/origin/master
 	private String numero;
-
-	@Column(name = "Nombre_asociacion")
-	private String nombreAs;
-
-	@Column(name = "Verrificado")
-	private String verificado;
-<<<<<<< HEAD
 	
-	@OneToOne
-=======
-
+	@Column(name="Nombre_asociacion")
+	private String nombreAs;
+	
+	@Column(name="Verrificado")
+	private String verificado;
+	
 	@ManyToOne
->>>>>>> refs/remotes/origin/master
 	@JoinColumns({
 		@JoinColumn(name="holder_idtype",referencedColumnName="identification_type"),
 		@JoinColumn(name="holder_idnumber",referencedColumnName="identification_number")
@@ -76,6 +58,10 @@ public class AsociacionCuentas implements Serializable {
 	public AsociacionCuentas() {
 		super();
 	}
+
+
+
+	
 
 	public AsociacionCuentas(String numeroId, String tipoId, String nombreTitular, Banco banco, String numero,
 			String nombreAs, String verificado, Customer cliente) {
@@ -90,71 +76,145 @@ public class AsociacionCuentas implements Serializable {
 		this.cliente = cliente;
 	}
 
+
+
+
+
 	public String getNumeroId() {
 		return numeroId;
 	}
+
+
+
+
 
 	public void setNumeroId(String numeroId) {
 		this.numeroId = numeroId;
 	}
 
+
+
+
+
 	public String getTipoId() {
 		return tipoId;
 	}
+
+
+
+
 
 	public void setTipoId(String tipoId) {
 		this.tipoId = tipoId;
 	}
 
+
+
+
+
 	public String getNombreTitular() {
 		return nombreTitular;
 	}
+
+
+
+
 
 	public void setNombreTitular(String nombreTitular) {
 		this.nombreTitular = nombreTitular;
 	}
 
+
+
+
+
 	public Banco getBanco() {
 		return banco;
 	}
+
+
+
+
 
 	public void setBanco(Banco banco) {
 		this.banco = banco;
 	}
 
+
+
+
+
 	public String getNumero() {
 		return numero;
 	}
+
+
+
+
 
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
 
+
+
+
+
 	public String getNombreAs() {
 		return nombreAs;
 	}
+
+
+
+
 
 	public void setNombreAs(String nombreAs) {
 		this.nombreAs = nombreAs;
 	}
 
+
+
+
+
 	public String getVerificado() {
 		return verificado;
 	}
+
+
+
+
 
 	public void setVerificado(String verificado) {
 		this.verificado = verificado;
 	}
 
+
+
+
+
 	public Customer getCliente() {
 		return cliente;
 	}
+
+
+
+
 
 	public void setCliente(Customer cliente) {
 		this.cliente = cliente;
 	}
 
-	
-	
 
+
+
+
+	public static String getListraAsociaciones() {
+		return LISTRA_ASOCIACIONES;
+	}
+
+	
+	
+	
+	
+	
 }
